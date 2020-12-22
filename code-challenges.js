d7fe7fec0d04439e1fa0ccfb70a8a63a2c59a874
M-ram224 <mattramirez2011@gmil.com>
2020-12-22 15:38:02 -0800

    Assessment completed

--- code-challenges.js
+++ code-challenges.js
@@ -1,18 +1,56 @@
 // ASSESSMENT 6: JavaScript Coding Practical Questions
 
-
 // --------------------1) Create a function that takes in an array of objects and returns a sentence about each person with their names capitalized.
 
 var people = [
   { name: "ford prefect", occupation: "hitchhiker" },
   { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
-  { name: "arthur dent", occupation: "radio employee" }
+  { name: "arthur dent", occupation: "radio employee" },
 ]
+
+// function takes an array of objects
+const personJob = (arr) => {
+  // nameArr takes the variable array of people then extracted the names into an array with only the names
+  let nameArr = arr.map((val) => {
+    console.log(val.name)
+    // local var splitting the names up
+    let splitMap = val.name.split(" ")
+    console.log(splitMap)
+    // returning and mapping through the split names, setting the first letter to be uppercase then making a slice at index 1 to not uppercase everything
+    // Then .joining the "strings" again in a str ${interpolation} where val = names and occupation = occupation
+    return (
+      splitMap.map((val) => val[0].toUpperCase() + val.slice(1)).join(" ") +
+      ` is a ${val.occupation}`
+    )
+  })
+  // returning the new str ${interpolated} sentences.
+  return nameArr
+}
+console.log(personJob(people))
 // Expected output example: "Ford Prefect is a hitchhiker." "Zaphod Beeblebrox is a president of the galaxy." "Arthus Dent is a radio employee."
 
+// *****************************************************************
+// From here down was my plan to map three times through the array, but had to scrap some of it, I will leave it until it is reviewed then I will beautify this, I put a lot of time into it prior to getting some help.
+// splitMap array to extract names to be seperated into their own array
+// let splitMap = val.name.map((val) => {
+//   return val.split(" ")
+// })
+// console.log(splitMap)
+// // third map capitalMap to extract the single letter and capitalize it
+// let capitalMap = splitMap.map((val) => {
+//   console.log(val)
+//   return val.map((val) => {
+//     // capoitalize the first indexed letters val[0] and disconnect the rest of the name at slice(1)
+//     return val[0].toUpperCase() + val.slice(1) + ` is a ${val.occupation}`
+//   })
+// })
+// what about creating a new value and pushing the now capitalized letters into the sentence with a str interpolation
+// let capitalMap =
+// if (capitalMap === charAt(0))
+//   return "Ford Prefect is a hitchhiker." "Zaphod Beeblebrox is a president of the galaxy." "Arthus Dent is a radio employee."
 
-
-
+// return capitalMap
+// return a `str ${} of all the key:value pairs`
 
 // --------------------2) Create a function that takes in a mixed data array and returns an array of only the REMAINDER of the numbers when divided by 3.
 
@@ -21,12 +59,36 @@ var testingArray1 = [23, "Heyyyy!", 45, -9, 0, "Yo", false]
 var testingArray2 = [5, "Hola", 43, -34, "greetings", true]
 // Expected output: [ 2, 1, -1 ]
 
-
-
-
-
+// function takes an arr
+const remOnly = (arr) => {
+  // filter through arr and find the typeof numbers only
+  let numArr = arr.filter((val) => {
+    return typeof val === "number"
+  })
+  // map through the numbers only array
+  return numArr.map((arr) => {
+    // divide nums by %3
+    return arr % 3
+    // return num remainder
+  })
+}
+console.log(remOnly(testingArray1))
+console.log(remOnly(testingArray2))
 // --------------------3) Create a function that takes in two arrays as arguments returns one array with no duplicate values.
 
 var testingArray3 = [3, 7, "hi", 10, 3, "hello", 4, "hi"]
 var testingArray4 = [7, "hi", 3, 1, "hi", 4, "hello", 4, 7]
 // Expected output: [ 3, 7, "hi", 10, "hello", 4, 1 ]
+
+// create a function that takes in two arrays
+const noDups = (arr, arr1) => {
+  // set a new local var largeArr up with the two smaller arrays concated together
+  let largeArr = arr.concat(arr1)
+  // return the largeArr filetred by the val and its index
+  return largeArr.filter((val, i) => {
+    // console.log(val, i)
+    // only returning the values that are strictly equal to the index
+    return largeArr.indexOf(val) === i
+  })
+}
+console.log(noDups(testingArray3, testingArray4))
--- rails-commenting.rb
+++ rails-commenting.rb
@@ -6,25 +6,25 @@
 
 # FILE: app/controller/blog_posts_controller.rb
 
-# 1)
+# 1) The BlogPostsController is the controller for all of the methods to be used in the application so we can use it on the web application.
 class BlogPostsController < ApplicationController
   def index
-    # 2)
+    # 2) @posts is an instance variable setup inside of our index(which routes as a get) to access all of our BlogPosts models previously made.
     @posts = BlogPost.all
   end
 
   def show
-    # 3)
+    # 3) @post is an instance variable setup inside of our show(Which routes as a get) to access only a single model in BlogPost using .find and the params that recieves info from the rails server of an :id.
     @post = BlogPost.find(params[:id])
   end
 
-  # 4)
+  # 4) The method defined as new(which routes as a get) is made to create and post new information in our web application by the user.
   def new
     @post = Post.new
   end
 
   def create
-    # 5)
+    # 5) @post instance variable is being setup inside of our method create(which routes as a post) to create posts in the web application the using a strong params to keep out unwanted hacker.
     @post = BlogPost.create(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -39,7 +39,7 @@ class BlogPostsController < ApplicationController
 
   def update
     @post = BlogPost.find(params[:id])
-    # 6)
+    # 6) @post is made to update(which routes to put and patch) a post while still using the security of strong params to keep hackers out.
     @post.update(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -53,15 +53,15 @@ class BlogPostsController < ApplicationController
     if @post.destroy
       redirect_to blog_posts_path
     else
-      # 7)
+      # 7) This else is here to redirect you to the show page if you dont end up deleting anything.
       redirect_to blog_post_path(@post)
     end
   end
 
-  # 8)
+  # 8) Private is a rails protective method where anything below its line can only be called inside of the class. Below this is where the strong params lives providing strong security to our web application from hackers.
   private
   def blog_post_params
-    # 9)
+    # 9) params is requiring that the attributes being passed into :blog_post to be present in the create or update methods. Then .permit allows :title and :content to be present but attributes that arent listed will not be allowed to be present.
     params.require(:blog_post).permit(:title, :content)
   end
 
@@ -71,6 +71,6 @@ end
 # FILE: app/models/blog_post.rb
 
 class BlogPost < ApplicationRecord
-  # 10)
+  # 10) has_many is giving the blog the ability to have comments in the scope of the application.
   has_many :comments
 end
